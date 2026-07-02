23b645eb18066e803728a3bae79986c7767b7586
minhnhutZzz <daominhnhut2017@gmail.com>
2026-07-02 07:54:25 +0700

    hot fix: gui mail

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -8,6 +8,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const sendOTPEmail = async (toEmail, userName, otpCode) => {
